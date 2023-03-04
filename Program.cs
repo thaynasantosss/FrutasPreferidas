14250e1733eb877dc329a28b5ea12b61d38aedeb
thaynasantosss <thayna.2015.09@gmail.com>
2023-03-03 21:34:46 -0300

    Remove comentário

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("--- Minhas frutas preferidas ---");
+﻿Console.WriteLine("--- Minhas frutas preferidas ---");
 Console.WriteLine();
 Console.WriteLine("1- Laranja");
 Console.WriteLine("2- Melancia");
